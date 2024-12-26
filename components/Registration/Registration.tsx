9ed80fe15e76729da49827f6db6934168bb14a73
Felipe Muner <felipe.muner@gmail.com>
2024-12-26 22:09:41 +0700

    feat: registration

--- components/Registration/Registration.tsx
+++ components/Registration/Registration.tsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React from 'react';
 
-const Hero: React.FC = () => {
+const Registration = () => {
   return (
-    <section className="bg-brand-background-1 pb-[70px] pt-[100px] w-full">
-      hero
+    <section className="bg-cover bg-center" style={{ backgroundImage: "url('img/banner-bg.jpg')" }}>
+      <div className="container mx-auto">
+        <div className="row">
+          <div className="col-lg-12 text-center">
+            <div className="bs-text">
+              <h2 className="text-4xl font-bold text-white uppercase">registration now to get more deals</h2>
+              <div className="text-brand-gray-light font-bold uppercase mt-4">Where health, beauty and fitness meet.</div>
+              <a href="#" className="inline-block text-sm py-4 px-8 text-white bg-brand-orange uppercase font-bold mt-6 transition duration-300 hover:bg-brand-orange">Appointment</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
 
-export default Hero;
+export default Registration;
