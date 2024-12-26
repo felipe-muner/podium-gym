cbe02cbb4dc1fc7e7722e408bde3e1311709b4a0
Felipe Muner <felipe.muner@gmail.com>
2024-12-26 21:06:56 +0700

    feat: new pages

---
+++ app/about-us/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutUs() {
+  return (
+    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <div><h1>AboutUs page</h1></div>
+    </main>
+  );
+}
---
+++ app/pricing/page.tsx
@@ -0,0 +1,7 @@
+export default function Pricing() {
+  return (
+    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <div>Pricing page</div>
+    </main>
+  );
+}
