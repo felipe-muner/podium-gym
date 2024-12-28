317243a907d81ffa188bfcfa02923cfa05b95cc5
Felipe Muner <felipe.muner@gmail.com>
2024-12-28 21:31:40 +0700

    feat: timetable initial component

--- app/classes/page.tsx
+++ app/classes/page.tsx
@@ -1,9 +1,11 @@
 import { Breadcrumb } from "@/components/Breadcrumb";
+import { Timetable } from "@/components/Timetable";
 
 export default function Classes() {
   return (
     <main className="flex flex-col">
       <Breadcrumb route={'Classes'} />      
+      <Timetable />
     </main>
   );
 }
---
+++ components/Timetable/Timetable.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const Timetable: React.FC = () => {
+  return (
+    <section className="flex justify-center">
+      timetable
+    </section>
+  )
+}
+
+export default Timetable;
