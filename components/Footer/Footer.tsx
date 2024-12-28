f338c017eba84c619b749fe2ac73de56d1211c72
Felipe Muner <felipe.muner@gmail.com>
2024-12-28 11:21:46 +0700

    feat: final version Footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,8 +1,8 @@
 import { Facebook, Twitter, Youtube, Instagram, Mail } from "lucide-react";
-import { APP_NAME } from "@/constants";
 import Link from "next/link";
-import { ContactData } from "../ContactData";
+import { APP_NAME } from "@/constants";
 import Image from "next/image";
+import { ContactData } from "@/components/ContactData";
 
 const Footer: React.FC = () => {
   return (
@@ -12,87 +12,109 @@ const Footer: React.FC = () => {
         <div className="container mx-auto px-4">
           {/* ROW */}
           <div className="flex flex-wrap -mx-4">
-
             {/* col-lg-4 (About) */}
             <div className="w-full px-4 mb-8 lg:mb-0 lg:w-1/3">
               <div>
-                <a href="#">
+                <Link href="#">
                   <Image
                     src="/img/logo.png"
                     alt={APP_NAME}
                     width={182}
                     height={41}
                   />
-                </a>
+                </Link>
               </div>
-              <p className="text-brand-gray-light leading-7 mt-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore dolore magna aliqua endisse
-                ultrices gravida lorem.
+              <p className="text-brand-gray-light text-sm font-mulish leading-7 mt-8">
+                Peak Performance Gym fosters strength, endurance, and balance through equipment,
+                trainers, and workouts, empowering everyone to achieve a healthier lifestyle.
               </p>
               <div className="flex space-x-4 mt-4">
-                <a href="#" className="text-brand-gray-light text-sm">
+                <Link
+                  href="#"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-brand-gray-light text-sm"
+                >
                   <Facebook size={16} />
-                </a>
-                <a href="#" className="text-brand-gray-light text-sm">
+                </Link>
+                <Link
+                  href="#"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-brand-gray-light text-sm"
+                >
                   <Twitter size={16} />
-                </a>
-                <a href="#" className="text-brand-gray-light text-sm">
+                </Link>
+                <Link
+                  href="#"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-brand-gray-light text-sm"
+                >
                   <Youtube size={16} />
-                </a>
-                <a href="#" className="text-brand-gray-light text-sm">
+                </Link>
+                <Link
+                  href="#"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-brand-gray-light text-sm"
+                >
                   <Instagram size={16} />
-                </a>
-                <a href="#" className="text-brand-gray-light text-sm">
+                </Link>
+                <Link
+                  href="mailto:yourgym@example.com"
+                  className="text-brand-gray-light text-sm"
+                >
                   <Mail size={16} />
-                </a>
+                </Link>
               </div>
+
             </div>
 
             {/* col-lg-2 col-md-3 col-sm-6 (Useful Links) */}
             <div className="w-full px-4 mb-8 lg:mb-0 sm:w-1/2 md:w-1/4 lg:w-1/6">
-              <h4 className="text-white font-semibold mb-4">Useful Links</h4>
-              <ul className="space-y-2">
+              <h4 className="text-white font-semibold text-2xl mb-4">Useful Links</h4>
+              <ul className="space-y-2 font-mulish leading-6">
                 <li>
-                  <a href="/about-us" className="text-brand-gray-light text-sm">
+                  <Link href="/about-us" className="text-brand-gray-light text-sm">
                     About us
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href="/blog" className="text-brand-gray-light text-sm">
+                  <Link href="/blog" className="text-brand-gray-light text-sm">
                     Blog
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href="/classes" className="text-brand-gray-light text-sm">
+                  <Link href="/classes" className="text-brand-gray-light text-sm">
                     Classes
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
 
             {/* col-lg-2 col-md-3 col-sm-6 (Support) */}
             <div className="w-full px-4 mb-8 lg:mb-0 sm:w-1/2 md:w-1/4 lg:w-1/6">
-              <h4 className="text-white font-semibold mb-4">Support</h4>
-              <ul className="space-y-2">
+              <h4 className="text-white font-semibold text-2xl mb-4">Support</h4>
+              <ul className="space-y-2 font-mulish leading-6">
                 <li>
-                  <a href="/contact" className="text-brand-gray-light text-sm">
+                  <Link href="/contact" className="text-brand-gray-light text-sm">
                     Contact
-                  </a>
+                  </Link>
                 </li>
                 <li>
-                  <a href="/calculate-bmi" className="text-brand-gray-light text-sm">
+                  <Link href="/calculate-bmi" className="text-brand-gray-light text-sm">
                     Calculate BMI
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
 
             {/* col-lg-4 col-md-6 (Tips & Guides) */}
             <div className="w-full px-4 md:w-1/2 lg:w-1/3">
-              <h4 className="text-white font-semibold mb-4">Tips & Guides</h4>
+              <h4 className="text-white font-semibold text-2xl mb-4">Tips & Guides</h4>
               <div className="space-y-6">
-                <div className="border-b border-brand-gray-dark pb-4">
+                <div className="border-b border-[#1a1a1a] pb-4">
                   <h6 className="mb-2">
                     <a
                       href="#"
@@ -101,7 +123,7 @@ const Footer: React.FC = () => {
                       Physical fitness may help prevent depression, anxiety
                     </a>
                   </h6>
-                  <ul className="text-xs text-brand-gray-darker">
+                  <ul className="text-xs text-brand-gray-darker font-mulish">
                     <li>3 min read</li>
                   </ul>
                 </div>
@@ -115,7 +137,7 @@ const Footer: React.FC = () => {
                       up...
                     </a>
                   </h6>
-                  <ul className="text-xs text-brand-gray-darker">
+                  <ul className="text-xs text-brand-gray-darker font-mulish">
                     <li>3 min read</li>
                   </ul>
                 </div>
@@ -127,16 +149,16 @@ const Footer: React.FC = () => {
           {/* COPYRIGHT SECTION */}
           <div className="mt-12 border-t border-brand-gray-dark pt-6 text-center">
             <p className="text-sm text-brand-gray-light">
-              Copyright &copy; {new Date().getFullYear()} All rights
-              reserved | This template is made by{" "}
-              <a
+              Copyright &copy; {new Date().getFullYear()}
+              All rights reserved | This template is made by{" "}
+              <Link
                 href="https://felipemuner.com/portfolio"
                 target="_blank"
-                rel="noopener noreferrer"
+                rel="noreferrer"
                 className="text-brand-orange"
               >
                 Felipe Muner
-              </a>
+              </Link>
             </p>
           </div>
         </div>
