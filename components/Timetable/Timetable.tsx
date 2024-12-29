b3a4eba19835a1e6471d891495f6568e05500af0
Felipe Muner <felipe.muner@gmail.com>
2024-12-29 17:59:46 +0700

    feat: amazing first timetable component version

--- components/Timetable/Timetable.tsx
+++ components/Timetable/Timetable.tsx
@@ -1,12 +1,102 @@
-import React from "react"
+import React from 'react';
 
+const Timetable = () => {
+  const timetable = [
+    {
+      time: '6.00am - 8.00am',
+      days: [
+        { name: 'WEIGHT LOOSE', instructor: 'RLefew D. Loee', type: 'dark' },
+        { name: 'Cardio', instructor: 'RLefew D. Loee' },
+        { name: 'Yoga', instructor: 'Keaf Shen', type: 'dark' },
+        { name: 'Fitness', instructor: 'Kimberly Stone' },
+        {},
+        { name: 'Boxing', instructor: 'Rachel Adam' },
+        { name: 'Body Building', instructor: 'Robert Cage', type: 'dark' },
+      ],
+    },
+    {
+      time: '10.00am - 12.00am',
+      days: [
+        {},
+        { name: 'Fitness', instructor: 'Kimberly Stone', type: 'dark' },
+        { name: 'WEIGHT LOOSE', instructor: 'RLefew D. Loee' },
+        { name: 'Cardio', instructor: 'RLefew D. Loee', type: 'dark' },
+        { name: 'Body Building', instructor: 'Robert Cage' },
+        { name: 'Karate', instructor: 'Donald Grey', type: 'dark' },
+        {},
+      ],
+    },
+    {
+      time: '5.00pm - 7.00pm',
+      days: [
+        { name: 'Boxing', instructor: 'Rachel Adam', type: 'dark' },
+        { name: 'Karate', instructor: 'Donald Grey' },
+        { name: 'Body Building', instructor: 'Robert Cage', type: 'dark' },
+        {},
+        { name: 'Yoga', instructor: 'Keaf Shen', type: 'dark' },
+        { name: 'Cardio', instructor: 'RLefew D. Loee' },
+        { name: 'Fitness', instructor: 'Kimberly Stone', type: 'dark' },
+      ],
+    },
+    {
+      time: '7.00pm - 9.00pm',
+      days: [
+        { name: 'Cardio', instructor: 'RLefew D. Loee' },
+        {},
+        { name: 'Boxing', instructor: 'Rachel Adam' },
+        { name: 'Yoga', instructor: 'Keaf Shen', type: 'dark' },
+        { name: 'Karate', instructor: 'Donald Grey' },
+        { name: 'Boxing', instructor: 'Rachel Adam', type: 'dark' },
+        { name: 'WEIGHT LOOSE', instructor: 'RLefew D. Loee' },
+      ],
+    },
+  ];
 
-const Timetable: React.FC = () => {
   return (
-    <section className="flex justify-center">
-      timetable
+    <section className="overflow-x-auto bg-brand-background-2 p-4 container mx-auto my-28">
+      <table className="min-w-full border-collapse border border-brand-gray-darker text-center">
+        <thead>
+          <tr className="bg-brand-orange">
+            <th className="p-3 text-sm text-white">&nbsp;</th>
+            <th className="p-3 text-sm text-white">Monday</th>
+            <th className="p-3 text-sm text-white">Tuesday</th>
+            <th className="p-3 text-sm text-white">Wednesday</th>
+            <th className="p-3 text-sm text-white">Thursday</th>
+            <th className="p-3 text-sm text-white">Friday</th>
+            <th className="p-3 text-sm text-white">Saturday</th>
+            <th className="p-3 text-sm text-white">Sunday</th>
+          </tr>
+        </thead>
+        <tbody>
+          {timetable.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              <td className="border border-brand-gray-darker bg-brand-black p-3 text-sm text-brand-orange">
+                {row.time}
+              </td>
+              {row.days.map((day, dayIndex) => (
+                <td
+                  key={dayIndex}
+                  className={`border border-brand-gray-darker p-3 text-sm ${day.type === 'dark' ? 'bg-brand-background-1' : 'bg-brand-background-2'
+                    } hover:bg-brand-orange transition`}
+                >
+                  {day.name ? (
+                    <div>
+                      <h5 className="mb-2 text-sm font-semibold uppercase text-white">
+                        {day.name}
+                      </h5>
+                      <span className="text-xs text-brand-gray-medium">
+                        {day.instructor}
+                      </span>
+                    </div>
+                  ) : null}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </section>
-  )
-}
+  );
+};
 
 export default Timetable;
