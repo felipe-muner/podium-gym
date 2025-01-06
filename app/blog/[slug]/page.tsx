30a7ad7f4c934d5897254e3d7f98d299d9287258
Felipe Muner <felipe.muner@gmail.com>
2025-01-06 09:06:23 +0700

    feat: new attempt to fix

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -4,42 +4,41 @@ import { blogPosts } from "@/components/Blog/data";
 import { Breadcrumb } from "@/components/Breadcrumb";
 
 interface BlogPostPageProps {
-  params: {
+  params: Promise<{
     slug: string;
-  };
+  }>;
 }
 
-// const BlogPostPage: React.FC<BlogPostPageProps> = ({ params }) => {
-async function BlogPostPage({ params }: BlogPostPageProps) {
-  const { slug } = await params
+const BlogPostPage = async (props: BlogPostPageProps) => {
+  const params = await props.params;
+  const { slug } = params; // Extract the slug from params
   const post = blogPosts.find((p) => p.slug === slug);
 
   if (!post) {
-    notFound(); // Display a 404 page if the post is not found
+    notFound(); // Trigger a 404 page if the post is not found
   }
 
   return (
     <>
       {/* Breadcrumb */}
-      <Breadcrumb
-        route="Blog Post"
-      />
+      <Breadcrumb route="Blog Post" />
 
       {/* Blog Post Content */}
       <div className="bg-brand-background-2 text-brand-gray-light min-h-screen py-8 mt-20">
         <div className="max-w-4xl mx-auto px-4">
           {/* Title Section */}
           <h1 className="text-5xl font-extrabold text-white mb-6 leading-tight">
-            {post?.title}
+            {post.title}
           </h1>
           <p className="text-sm text-brand-gray-medium mb-8">
-            Published on {post?.date} • <span className="text-brand-orange">{post?.readTime} min read</span>
+            Published on {post.date} •{" "}
+            <span className="text-brand-orange">{post.readTime} min read</span>
           </p>
 
           {/* Content Section */}
           <div
             className="text-lg font-mulish leading-relaxed text-brand-gray-light tracking-wide space-y-6"
-            dangerouslySetInnerHTML={{ __html: post?.content ?? "" }}
+            dangerouslySetInnerHTML={{ __html: post.content }}
           />
         </div>
       </div>
