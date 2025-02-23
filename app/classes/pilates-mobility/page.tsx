a0f1294383ac77695846c581107428aaeaeaec9c
Felipe Muner <felipe.muner@gmail.com>
2025-02-23 21:10:32 +0700

    feat: pilates

--- app/classes/pilates-mobility/page.tsx
+++ app/classes/pilates-mobility/page.tsx
@@ -1,16 +1,40 @@
 import { Breadcrumb } from "@/components/Breadcrumb";
 import { getMetadata } from "@/lib/utils";
+import { Check } from "lucide-react";
 
 export const metadata = getMetadata({ routeName: "Pilates Mobility" });
+
 export default function PilatesMobility() {
   return (
     <>
       <Breadcrumb route={'Pilates Mobility'} isClass />   
-      <div className="flex flex-col gap-4 items-center justify-center p-4 my-20 text-brand-gray-medium container mx-auto max-w-[700px] font-mulish">
-        <h1 className="text-2xl font-bold text-center">Pilates Mobility</h1>
-        <p className="text-center">
-          Pilates Mobility is a low-impact exercise class that focuses on improving flexibility, strength, and balance. This class is designed to help you move more efficiently and with greater ease by targeting the muscles that support your spine and joints. Pilates Mobility is a great way to improve your posture, reduce your risk of injury, and enhance your overall well-being.
-        </p>
+      <div className="flex flex-col gap-4 items-start justify-center p-4 my-20 text-brand-gray-medium container mx-auto max-w-[700px] font-mulish">
+        <h1 className="text-3xl font-semibold text-white text-left">Pilates Mobility with Vanessa</h1>
+        <p>Pilates Mobility is a class designed to improve flexibility, strength, and body awareness through controlled movements that emphasize core engagement and proper alignment.</p>
+        <p>Using props like resistance bands, yoga balls, and magic circles, this class enhances joint mobility and stability, making it suitable for all fitness levels.</p>
+        
+        <p>Whether you&apos;re an elite athlete, post-natal, or recovering from injury, Pilates serves as the foundation for movement in daily life. With spinal health at its core, this practice strengthens deep stabilizing muscles, promotes postural balance, and enhances freedom of movement while toning the abdominals, waistline, thighs, and buttocks.</p>
+        
+        <p>Additionally, Pilates supports organ detoxification, breath activation, and improved muscle function, contributing to overall well-being.</p>
+        
+        <h2 className="text-3xl font-semibold mt-4 text-white">Key Benefits</h2>
+        <ul className="space-y-3">
+          <li className="flex items-start gap-2">
+            <Check size={20} className="text-brand-orange flex-shrink-0 mt-1" /> <span>Improved strength & stability</span>
+          </li>
+          <li className="flex items-start gap-2">
+            <Check size={20} className="text-brand-orange flex-shrink-0 mt-1" /> <span>Full-body toning & posture alignment</span>
+          </li>
+          <li className="flex items-start gap-2">
+            <Check size={20} className="text-brand-orange flex-shrink-0 mt-1" /> <span>Enhanced breath control</span>
+          </li>
+          <li className="flex items-start gap-2">
+            <Check size={20} className="text-brand-orange flex-shrink-0 mt-1" /> <span>Nervous system regulation</span>
+          </li>
+          <li className="flex items-start gap-2">
+            <Check size={20} className="text-brand-orange flex-shrink-0 mt-1" /> <span>Deeper body-mind connection</span>
+          </li>
+        </ul>
       </div>   
     </>
   );
