59e90e81df3e339eb2f1dd9a6720b8d03c0fdc5f
Felipe Muner <felipe.muner@gmail.com>
2025-02-23 21:04:47 +0700

    feat: more pages

--- app/the-club/overview/page.tsx
+++ app/the-club/overview/page.tsx
@@ -1,6 +1,6 @@
 import { Breadcrumb } from "@/components/Breadcrumb";
 import { getMetadata } from "@/lib/utils";
-import { Check } from "lucide-react";
+import { Check, Clock10, Clock7 } from "lucide-react";
 
 export const metadata = getMetadata({ routeName: "overview" });
 
@@ -8,10 +8,11 @@ export default function Overview() {
   return (
     <>
       <Breadcrumb route={'Overview'} isClass />
-      <div className="flex flex-col gap-4 items-center justify-center p-4 my-20 text-brand-gray-medium container mx-auto max-w-[700px] font-mulish">
-        <h1 className="text-2xl font-bold text-center">Experience Premium Fitness in the Heart of Koh Phangan</h1>
-        <p className="text-white font-semibold">Open daily from <span className="text-white">7:00 to 22:00</span></p>
-        <p className="">Spanning 1,000m² (5,400 sq. ft.), our modern fitness center is designed for those who prioritize performance, hygiene, and well-being.</p>
+      <div className="flex flex-col gap-4 items-start justify-center p-4 my-20 text-brand-gray-medium container mx-auto max-w-[700px] font-mulish">
+        <h1 className="text-3xl font-semibold text-white text-left">Experience Premium Fitness in the Heart of Koh Phangan</h1>
+        <p className="text-white font-semibold">Open daily from <span className="text-white flex items-center gap-1"><Clock7 className="text-brand-orange" size={20} /> 7:00 to <Clock10 className="text-brand-orange" size={20} /> 22:00</span></p>
+        
+        <p>Spanning 1,000m² (5,400 sq. ft.), our modern fitness center is designed for those who prioritize performance, hygiene, and well-being.</p>
         
         <ul className="space-y-3">
           <li className="flex items-start gap-2">
--- app/the-club/protein-bar-and-shop/page.tsx
+++ app/the-club/protein-bar-and-shop/page.tsx
@@ -2,10 +2,17 @@ import { Breadcrumb } from "@/components/Breadcrumb";
 import { getMetadata } from "@/lib/utils";
 
 export const metadata = getMetadata({ routeName: "Protein Bar and Shop" });
+
 export default function ProteinBarAndShop() {
   return (
     <>
-      <Breadcrumb route={'Protein bar and Shop'} isClass />
+      <Breadcrumb route={'Protein Bar and Shop'} isClass />
+      <div className="flex flex-col gap-4 items-start justify-center p-4 my-20 text-brand-gray-medium container mx-auto max-w-[800px] font-mulish">
+        <h1 className="text-3xl font-semibold mt-4 text-white">Refuel & Recover</h1>
+        <p>Elevate your fitness experience at our <strong className="text-white">bar and shop</strong>, where <strong className="text-white">health meets convenience</strong>. Enjoy <strong className="text-white">nutritious protein and vegan shakes</strong> to keep you fuelled and ready <strong className="text-white">before or after your workout</strong>.</p>
+        
+        <p>Looking to stock up? Our shop offers <strong className="text-white">bulk protein and a variety of supplements</strong> to support your fitness journey. <strong className="text-white">Stay strong, stay nourished!</strong></p>
+      </div>
     </>
   );
 }
