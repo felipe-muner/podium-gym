f6e3723c005146b5c69c23a9820fbfae3467c9d3
Felipe Muner <felipe.muner@gmail.com>
2025-02-24 16:26:07 +0700

    feat: reuse data for both component

--- components/OurPlan/OurPlan.tsx
+++ components/OurPlan/OurPlan.tsx
@@ -1,62 +1,6 @@
 import { TitleSection } from "@/components/TitleSection";
 import { cn } from "@/lib/utils";
-
-const membershipPlans = [
-  {
-    type: "Gym",
-    plans: [
-      {
-        category: "Gym / Steam / Ice-bath",
-        plans: [
-          { name: "Drop-in", price: "300 ฿" },
-          { name: "5-pass", price: "1,250 ฿" },
-          { name: "1-month", price: "1,900 ฿" },
-          { name: "3-month", price: "5,100 ฿" },
-          { name: "6-month", price: "9,000 ฿" },
-          { name: "12-month", price: "16,000 ฿" },
-        ],
-      },
-      {
-        category: "Fitness Classes",
-        plans: [
-          { name: "Drop-in", price: "300 ฿" },
-          { name: "5-pass", price: "1,250 ฿" },
-          { name: "1-month", price: "2,800 ฿" },
-        ],
-      },
-    ],
-  },
-  {
-    type: "Crossfit",
-    plans: [
-      {
-        category: "Group Training",
-        plans: [
-          { name: "Drop-in", price: "600 ฿" },
-          { name: "5-pass", price: "2,250 ฿" },
-          { name: "1-month", price: "4,200 ฿" },
-          { name: "3-month", price: "11,400 ฿" },
-          { name: "6-month", price: "21,600 ฿" },
-        ],
-      },
-      {
-        category: "Open-Gym",
-        plans: [
-          { name: "Drop-in", price: "450 ฿" },
-          { name: "1-month", price: "3,000 ฿" },
-        ],
-      },
-      {
-        category: "Group Training + Open-Gym",
-        plans: [
-          { name: "1-month", price: "5,000 ฿" },
-          { name: "3-month", price: "13,500 ฿" },
-          { name: "6-month", price: "25,800 ฿" },
-        ],
-      },
-    ],
-  },
-];
+import { membershipPlans } from "./membershipData";
 
 interface OurPlanProps {
   className?: string;
